6351d0f7b4bbaa182f42b1d6bd9cc2bb4469b3d5
Oscar M Romero <omromero@CodePC.lan>
2022-05-14 19:37:33 -0400

    created items model and reference item as resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :inventories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :todo_lists do
+    resources :items
+   end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
