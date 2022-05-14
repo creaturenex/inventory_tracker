b681530f18725500027ca3ee0e75ce427a4e79a5
Oscar M Romero <omromero@CodePC.lan>
2022-05-14 19:26:26 -0400

    initial commit

---
+++ db/migrate/20220514232326_create_inventories.rb
@@ -0,0 +1,11 @@
+class CreateInventories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :inventories do |t|
+      t.string :name
+      t.text :location
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
